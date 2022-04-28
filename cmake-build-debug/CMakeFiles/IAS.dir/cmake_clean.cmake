f1b12ee802ba51f4787030a42a66d2ce21170772
LuannFelipe <luannf1020@gmail.com>
2022-04-28 19:57:38 -0300

    inicio da separação do programa

---
+++ cmake-build-debug/CMakeFiles/IAS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IAS.dir/main.c.obj"
+  "CMakeFiles/IAS.dir/main.c.obj.d"
+  "IAS.exe"
+  "IAS.exe.manifest"
+  "IAS.pdb"
+  "libIAS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IAS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -39,7 +39,9 @@ void decodifica()
        //xxxx xxxx xxx0 01xx xxxx xxxx xxxx xxxx
        //0000 0000 0001 1100 0000 0000 0000 0000
        //   0    0    1    C    0    0    0    0
-       ro1 = (mbr & 0x001c0000) >> 16;
+
+       //0000 0011 0000 0100 0000 0000 0000 0000
+       ro1 = (mbr & 0x001c0000) >> 18;
     } else if(ir == 0xB)
    {
        //NOT
@@ -71,6 +73,8 @@ void decodifica()
        mar = mbr & 0x001fffff;
 
 
+
+
    } else if(ir >= 0x15 && ir <= 0x1B)
    {
        //MOVI
@@ -80,9 +84,11 @@ void decodifica()
 
        ro0 = (mbr & 0x00E00000) >> 21;
 
-       mar = mbr & 0x001fffff;
+       //xxxx xxxx xxxy yyyy yyyy yyyy yyyy yyyy
+       //0000 0000 0001 1111 1111 1111 1111 1111
+       //   0    0    1    f    f    f    f    f
 
-       imm = memoria[mar];
+       imm = mbr & 0x001fffff;
    }
 }
 void executa()
@@ -207,7 +213,13 @@ void executa()
             break;
         case 0x13:
             //ld
-            reg[ro0] = memoria[mar];
+            reg[ro0] = memoria[mar++];
+            for(int i = 1; i < 4; i++)
+            {
+                reg[ro0] = reg[ro0] << 8;
+                reg[ro0] = reg[ro0] | memoria[mar++];
+            }
+
             pc += 4;
             break;
         case 0x14:
@@ -233,7 +245,7 @@ void executa()
             //xxxx xxxx xxxx xxxx xxxx xxxx yyyy yyyy
             //0000 0000 0000 0000 0000 0000 1111 1111
             //   0    0    0    0    f    f     0   0
-            memoria[mar++] = reg[ro0] & 0x000000ff;
+            memoria[mar] = reg[ro0] & 0x000000ff;
 
             pc += 4;
 
@@ -287,6 +299,15 @@ void lerTexto()
     char *pl;
     char str[50];
     int count = 0;
+
+    int inicio;
+    char tipo;
+    unsigned int palavra;
+    unsigned char opcode;
+    unsigned int reg1;
+    unsigned int reg2;
+    unsigned int menOuImm;
+
     arq = fopen("programa.txt", "r");
 
     if (NULL == arq) {
@@ -299,6 +320,38 @@ void lerTexto()
            while(count < 3)
            {
                printf("token:%s\n",pl);
+               if(count == 0)
+               {
+                   //inicio
+                   inicio = (int) strtol(pl,NULL,16);
+                   printf("%d --- %x\n",inicio,inicio);
+               } else if(count == 1)
+               {
+                   //tipo
+                   tipo = *pl;
+                   printf("%c\n",tipo);
+               } else{
+                   if(tipo == 'i')
+                   {
+                       pl = strtok(pl, "r, ");
+                       //palavra
+                       if(strcmp(pl,"nop") == 0)
+                       {
+                           opcode = 0x01;
+                       } else if(strcmp(pl,"ld") == 0)
+                       {
+                           opcode = 0x13;
+                           pl = strtok(NULL,"r, ");
+                           reg1 = (int) strtol(pl,NULL, 16);
+                           pl = strtok(NULL,"r, ");
+                           menOuImm = (int) strtol(pl,NULL, 16);
+                       }
+                       return;
+                   } else{
+                       palavra = (int) strtol(pl,NULL,16);
+                       printf("%d --- %x\n",palavra,palavra);
+                   }
+               }
                pl = strtok(NULL,";");
                count++;
            }
@@ -306,27 +359,102 @@ void lerTexto()
            printf("\n");
         }
     }
-
-
     fclose(arq);
+}
 
+int criar_palavra(unsigned int palavra, unsigned char opcode, unsigned int reg1, unsigned int reg2, unsigned int menOuImm)
+{
+    int novaPalavra;
+
+    if(ir >= 0x2 && ir <= 0xA)
+    {
+        //ADD,SUB,MUL,DIV,CMP,AND,OR,XOR
+
+    } else if(ir == 0xB)
+    {
+        //NOT
+
+    } else if(ir >=0xC && ir <= 0x12)
+    {
+        //JE, JE JL JLE JG JGE JMP
+
+    } else if(ir >= 0x13 && ir <= 0x14)
+    {
+        //LD, ST
+    } else if(ir >= 0x15 && ir <= 0x1B)
+    {
+        //MOVI
+
+    }
+
+    return palavra;
 }
 
 int main() {
 
+  //  lerTexto();
+//
+//  pc = 0;
+//  memoria[0] = 0x13;
+//  memoria[1] = 0x00;
+//  memoria[2] = 0x00;
+//  memoria[3] = 0x10;
+//
+//  memoria[4] = 0x13;
+//  memoria[5] = 0x20;
+//  memoria[6] = 0x00;
+//  memoria[7] = 0x14;
+//
+//  memoria[0x08] = 0x16;
+//  memoria[0x09] = 0x00;
+//  memoria[0x0a] = 0x00;
+//  memoria[0x0b] = 0x14;
+//
+//  memoria[0x0c] = 0x00;
+//  memoria[0x0d] = 0x00;
+//  memoria[0x0e] = 0x00;
+//  memoria[0x0f] = 0x00;
+//
+//  memoria[0x10] = 0x00;
+//  memoria[0x11] = 0x00;
+//  memoria[0x12] = 0x00;
+//  memoria[0x13] = 0x0f;
+//
+//  memoria[0x14] = 0x00;
+//  memoria[0x15] = 0x00;
+//  memoria[0x16] = 0x00;
+//  memoria[0x17] = 0x08;
+//
+//  busca();
+//  decodifica();
+//  while(ir != 0x00)
+//  {
+//
+//      executa();
+//      printf("mbr -- %x\n", mbr);
+//      printf("mar -- %x\n", mar);
+//      printf("ir -- %x\n", ir);
+//      printf("ro0 -- %x\n", ro0);
+//      printf("ro1 -- %x\n", ro1);
+//      printf("imm -- %x\n", imm);
+//      printf("pc -- %x\n", pc);
+//      printf("e -- %x\n", e);
+//      printf("l -- %x\n", l);
+//      printf("g -- %x\n", g);
+//      printf("r0 -- %x\n", reg[0]);
+//      printf("r1 -- %x\n", reg[1]);
+//      printf("r2 -- %x\n", reg[2]);
+//      printf("r3 -- %x\n", reg[3]);
+//      printf("r4 -- %x\n", reg[4]);
+//      printf("r5 -- %x\n", reg[5]);
+//      printf("r6 -- %x\n", reg[6]);
+//      printf("r7 -- %x\n", reg[7]);
+//      printf("------------------------\n");
+//      busca();
+//      decodifica();
+//  }
+
     lerTexto();
-
-//    pc = 0;
-//    memoria[0] = 0x02;
-//    memoria[1] = 0xff;
-//    memoria[2] = 0;
-//    memoria[3] = 0x1f;
-//    busca();
-//    decodifica();
-
-    //printf("%x", ro1);
-    //0000 0010 1111 0000 0001 1111
-
     return 0;
 }
 
